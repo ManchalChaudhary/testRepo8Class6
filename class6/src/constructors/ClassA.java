fb1f59f57ca8d5d5cdce3af7b66beea0a032ca74
ManchalChaudhary <95185678+ManchalChaudhary@users.noreply.github.com>
2025-02-01 12:49:01 -0600

    Update ClassA.java

--- class6/src/constructors/ClassA.java
+++ class6/src/constructors/ClassA.java
@@ -3,7 +3,7 @@ package constructors;
 public class ClassA {
 
 	public static void main(String[] args) {
-		ClassC cC = new ClassC("Hello", 20);
+		ClassC cC = new ClassC("Hello", 40);
 		cC.C2(10);
 	}
 	public static void A1() {System.out.println("A1");}
